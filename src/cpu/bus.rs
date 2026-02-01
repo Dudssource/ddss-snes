3f98ab1737281f795d75f6d686db4e50b2c90e83
dudssource <dudssource@gmail.com>
2026-01-31 22:40:12 -0300

    fix: avoid stackoverflow on the bus boxed slice

--- src/cpu/bus.rs
+++ src/cpu/bus.rs
@@ -2,13 +2,13 @@ const STACK_POINTER_START: u16 = 0x1FF;
 const STACK_POINTER_END: u16 = 0x100;
 
 pub struct Bus {
-    work_ram: Box<[u8; 0xFFFF]>,
+    work_ram: Box<[u8]>,
 }
 
 impl Bus {
     pub fn new() -> Self {
         Self {
-            work_ram: Box::new([0; 0xFFFF]),
+            work_ram: vec![0u8; i32::MAX as usize].into_boxed_slice(),
         }
     }
 
